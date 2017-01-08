3273803ad3ef51589aeb8d20d5578fbcc6dd1c81
Colinas Maoling <colinas.maoling@t-online.de>
2017-01-08 09:30:19 +0100

    recalc dividendPerShare also when editing, set dividend to 0 in case no shares are present

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/transactions/AccountTransactionModel.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/transactions/AccountTransactionModel.java
@@ -23,7 +23,6 @@ import name.abuchen.portfolio.money.Values;
 import name.abuchen.portfolio.snapshot.ClientSnapshot;
 import name.abuchen.portfolio.snapshot.SecurityPosition;
 import name.abuchen.portfolio.ui.Messages;
-import name.abuchen.portfolio.ui.PortfolioPlugin;
 
 public class AccountTransactionModel extends AbstractModel
 {
@@ -162,8 +161,8 @@ public class AccountTransactionModel extends AbstractModel
 
         setShares(0);
         setFxGrossAmount(0);
-        setDividendAmount(0);
         setGrossAmount(0);
+        setDividendAmount(0);
         setTaxes(0);
         setFxTaxes(0);
         setNote(null);
@@ -201,7 +200,6 @@ public class AccountTransactionModel extends AbstractModel
         this.account = account;
         this.date = transaction.getDate();
         this.shares = transaction.getShares();
-        this.dividendAmount = 0; // TODO
         this.total = transaction.getAmount();
 
         // both will be overwritten if forex data exists
@@ -229,6 +227,7 @@ public class AccountTransactionModel extends AbstractModel
         });
 
         this.grossAmount = calculateGrossAmount4Total();
+	this.dividendAmount = calculateDividendAmount();
 
         // in case units have to forex gross value
         if (exchangeRate.equals(BigDecimal.ONE))
@@ -521,7 +520,7 @@ public class AccountTransactionModel extends AbstractModel
 
     protected long calculateDividendAmount()
     {
-        long result = -1;
+        long result = 0;
         if (shares > 0) {
             result = Math.round((fxGrossAmount * Values.Share.factor()) / shares);
         }
