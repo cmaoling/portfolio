3428dfb3ecde496823b8014223116c7cd85488f2
Andreas Buchen <andreas.buchen@gmail.com>
2018-10-13 08:17:00 +0200

    Updated data binding code to 2018-09 (1bdeccb4bf3e91b57b94a76191c23a2db49bbd06)

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/transactions/AccountTransactionDialog.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/transactions/AccountTransactionDialog.java
@@ -123,9 +123,10 @@ public class AccountTransactionDialog extends AbstractTransactionDialog // NOSON
         Label lblDate = new Label(editArea, SWT.RIGHT);
         lblDate.setText(Messages.ColumnDate);
         DatePicker valueDate = new DatePicker(editArea);
+        IObservableValue<?> targetDate = new SimpleDateTimeDateSelectionProperty().observe(valueDate.getControl());
         @SuppressWarnings("unchecked")
         IObservableValue<?> dateObservable = BeanProperties.value(Properties.date.name()).observe(model);
-        context.bindValue(new SimpleDateTimeDateSelectionProperty().observe(valueDate.getControl()), dateObservable);
+        context.bindValue(targetDate, dateObservable);
 
         // shares
 
@@ -202,9 +203,10 @@ public class AccountTransactionDialog extends AbstractTransactionDialog // NOSON
         Label lblNote = new Label(editArea, SWT.LEFT);
         lblNote.setText(Messages.ColumnNote);
         Text valueNote = new Text(editArea, SWT.BORDER);
+        IObservableValue<?> targetNote = WidgetProperties.text(SWT.Modify).observe(valueNote);
         @SuppressWarnings("unchecked")
         IObservableValue<?> noteObservable = BeanProperties.value(Properties.note.name()).observe(model);
-        context.bindValue(WidgetProperties.text(SWT.Modify).observe(valueNote), noteObservable);
+        context.bindValue(targetNote, noteObservable);
 
         //
         // form layout
