2adb0cc59cedae5bc759506985e2175f474a94a1
Ragas13 <marco.forum@t-online.de>
2019-09-07 21:43:18 +0200

    Created WebAccess class for centralize download of data
    Issue: #1227
    Signed-off-by: Ragas13 <marco.forum@t-online.de>
    [squasched commits]
    Signed-off-by: Andreas Buchen <andreas.buchen@gmail.com>

--- name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/HTMLTableQuoteFeed.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/HTMLTableQuoteFeed.java
@@ -30,6 +30,9 @@ import name.abuchen.portfolio.model.Security;
 import name.abuchen.portfolio.model.SecurityPrice;
 import name.abuchen.portfolio.money.Values;
 import name.abuchen.portfolio.online.QuoteFeed;
+import name.abuchen.portfolio.online.impl.variableurl.Factory;
+import name.abuchen.portfolio.online.impl.variableurl.urls.VariableURL;
+import name.abuchen.portfolio.util.OnlineHelper;
 import name.abuchen.portfolio.util.TextUtil;
 
 public class HTMLTableQuoteFeed extends QuoteFeed
