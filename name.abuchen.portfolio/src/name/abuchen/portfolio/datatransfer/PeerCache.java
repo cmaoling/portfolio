854ea2b798df97369ce799ec45ebaeb663043e8e
Colinas Maoling <colinas.maoling@t-online.de>
2022-03-06 11:06:30 +0100

    lookupPeerByName(..) now searches name as case-insensitive, lookup(..) values iban for search only if it is a valid IBAN and exchanges iban and name, if name is valid IBAN

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/PeerCache.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/PeerCache.java
@@ -16,6 +16,7 @@ import name.abuchen.portfolio.datatransfer.Extractor.Item;
 import name.abuchen.portfolio.datatransfer.Extractor.PeerItem;
 import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.Peer;
+import name.abuchen.portfolio.util.Iban;
 
 public class PeerCache
 {
@@ -43,30 +44,45 @@ public class PeerCache
     public Peer lookup(String iban, String name,
                     Supplier<Peer> creationFunction)
     {
-        List<String> attributes = Arrays.asList(iban, name);
-
-        // first: check the identifying attributes (IBAN)
-        for (int ii = 0; ii < 1; ii++)
+        if (Iban.isValid(name) && iban == null)
         {
-            String attribute = attributes.get(ii);
-
-            Peer peer = localMaps.get(ii).get(attribute);
-            if (peer == DUPLICATE_PEER_MARKER)
-                throw new IllegalArgumentException(MessageFormat.format(MESSAGES.get(ii), attribute));
-            if (peer != null)
-                return peer;
+            // parterName is a valid iban, so switch variables (enables a ParterName column to held IBANs as well)
+            iban = name;
+            name = null;
         }
+
+        if (!Iban.isValid(iban))
+            iban = Iban.IBANNUMBER_DUMMY;
+
+        List<String> attributes = Arrays.asList(iban, name);
+        // DEBUG System.err.println("PeerCache lookup: IBAN: " + iban + " valid? " + Iban.isValid(iban));  //$NON-NLS-1$  //$NON-NLS-2$
+
+        if (Iban.isValid(iban))
+            // first: check the identifying attributes (IBAN)
+            for (int ii = 0; ii < 1; ii++)
+            {
+                String attribute = attributes.get(ii);
+
+                Peer peer = localMaps.get(ii).get(attribute);
+                if (peer == DUPLICATE_PEER_MARKER)
+                    throw new IllegalArgumentException(MessageFormat.format(MESSAGES.get(ii), attribute));
+                if (peer != null)
+                    return peer;
+            }
 
         // second: check the name. But: even if the name matches, we also must
         // check that the identifying attributes do not differ. Why? Investment
         // instruments could have the same name but different ISINs.
-
         Peer peer = lookupPeerByName(iban, name);
+        // DEBUG System.err.println("PeerCache lookup: " + (peer != null?peer.toString():"<no peer>") + " - IBAN: " + (iban != null?iban:"<NULL>") + " Name: " + (name != null?name:"<NULL>")); //$NON-NLS-1$  //$NON-NLS-2$
         if (peer != null)
             return peer;
 
         peer = creationFunction.get();
-        peer.setIban(iban);
+        if (Iban.isValid(iban))
+            peer.setIban(iban);
+        else
+            peer.setIban(Iban.IBANNUMBER_DUMMY);
         peer.setName(name);
 
         for (int ii = 0; ii < localMaps.size(); ii++)
@@ -76,6 +92,7 @@ public class PeerCache
                 localMaps.get(ii).put(attribute, peer);
         }
 
+        // DEBUG System.err.println("PeerCache lookup: " + (peer != null?peer.toString()+"/"+peer:"<no peer>")); //$NON-NLS-1$  //$NON-NLS-2$
         return peer;
     }
 
@@ -83,11 +100,19 @@ public class PeerCache
     {
         Peer peer = localMaps.get(1).get(name);
 
+        // Ignore upper/lower case in Peer Name
+        if (peer == null || peer == DUPLICATE_PEER_MARKER)
+        {
+            for (String key : localMaps.get(1).keySet())
+                if (key.toLowerCase().equals(name.toLowerCase()))
+                    peer = localMaps.get(1).get(key);
+        }
+
         // allow imports by duplicate name
         if (peer == null || peer == DUPLICATE_PEER_MARKER)
             return null;
 
-        if (doNotMatchIfGiven(iban, peer.getIban()))
+        if (Iban.isValid(iban) && doNotMatchIfGiven(iban, peer.getIban()))
             return null;
         return peer;
     }
